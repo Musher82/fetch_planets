42aefdf5f9fcfcb0feb4b8ec6838adeb5a467805
Micah <mrusher@gmail.com>
2021-04-11 16:16:49 -0500

    chapter exercises

--- angular_practice/my-first-project/src/app/app.component.ts
+++ angular_practice/my-first-project/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'I Changed The Title';
+  name: string = 'Micah Usher';
+  itemList: string[] = ['item1', 'item2', 'item3', 'item4']
+  rectangle = {
+    length: 5, 
+    width: 6,
+    area: function() {
+      return this.length * this.width;
+    }
+  }
 }
